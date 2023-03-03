61df2bdc1aa36cc77687f6931ee323b79ff1258d
German Torres <germantorres@Germans-MacBook-Pro.local>
2023-03-03 11:42:00 -0600

    initial commit

---
+++ happyeat/src/main/java/com/gtm/happyeat/HappyeatApplication.java
@@ -0,0 +1,13 @@
+package com.gtm.happyeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyeatApplication.class, args);
+	}
+
+}
---
+++ happyeat/src/test/java/com/gtm/happyeat/HappyeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtm.happyeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
